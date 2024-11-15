1506995a9c2ae6ac261655f61c42b5a3120e6984
Abdullah Alashhab <abdullahalshhab@gmail.com>
2024-11-15 18:43:32 +0200

    final project

---
+++ src/components/Form.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { ComponentProps, useState } from "react";
+
+function Form({
+  onSubmit,
+  deleteAll,
+}: {
+  onSubmit: (title: string) => void;
+  deleteAll: () => void;
+}) {
+  const [title, setTitle] = useState("");
+
+  return (
+    <div className="flex flex-col w-[400px] gap-2 mx-auto mt-4">
+      <div className="flex items-center mx-auto gap-2">
+        <h2>Todos with Docker</h2>
+        <img src={"../docker.svg"} className="w-[40px]" alt="" />
+      </div>
+
+      <Input
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Button
+        onClick={() => {
+          onSubmit(title);
+          setTitle("");
+        }}
+      >
+        Add
+      </Button>
+      <Button variant={"destructive"} onClick={() => deleteAll()}>
+        Delete All
+      </Button>
+    </div>
+  );
+}
+
+type MarkDoneProps = ComponentProps<"button"> & {
+  id: string;
+  markDone: (id: string) => void;
+};
+
+function MarkDone({ id, markDone, disabled }: MarkDoneProps) {
+  return (
+    <Button disabled={disabled} className="w-fit" onClick={() => markDone(id)}>
+      Done {disabled ? "" : "?"}
+    </Button>
+  );
+}
+
+export { Form, MarkDone };
